94b335cd39ca913f95f4d452b905620887b4e130
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-22 20:54:51 +0200

    added LOGGER for checking if User user variable is null.

--- src/main/java/com/musichub/musichubapp/service/DbService.java
+++ src/main/java/com/musichub/musichubapp/service/DbService.java
@@ -1,15 +1,20 @@
 package com.musichub.musichubapp.service;
 
-import com.musichub.musichubapp.domain.User;
+import com.musichub.musichubapp.entities.User;
 import com.musichub.musichubapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class DbService {
+
+    private static final Logger LOGGER = Logger.getLogger(DbService.class.getName());
+
     @Autowired
     private UserRepository repository;
 
@@ -23,6 +28,9 @@ public class DbService {
     }
     public User saveUser(final User user){
 
+        if(user ==null){
+            LOGGER.log(Level.SEVERE, "User is null!");
+        }
         return repository.save(user);
     }
     public void deleteUser(Integer id){
