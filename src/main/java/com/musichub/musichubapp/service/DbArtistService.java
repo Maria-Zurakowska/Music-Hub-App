71826ea673e3c8075b32eb8146de8eab47d0311b
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-22 20:56:29 +0200

    removed unused content, added new methods: getAllArtistsGrades, getArtistGrade, saveArtistGrade, deleteArtistGrade

--- src/main/java/com/musichub/musichubapp/service/DbArtistService.java
+++ src/main/java/com/musichub/musichubapp/service/DbArtistService.java
@@ -1,9 +1,8 @@
 package com.musichub.musichubapp.service;
 
-// klasa służacą do pobierania, usuwania, modyfikowania itp. Artist w bazie danych (stworzę opcję dodawania artysty
-//do ulubionych i tym samym stworzę tabelę z ulubionymi artystami, więc musiałam też stworzyć osobną klasę Artist).
-
-import com.musichub.musichubapp.domain.Artist;
+import com.musichub.musichubapp.entities.Artist;
+import com.musichub.musichubapp.entities.ArtistGrade;
+import com.musichub.musichubapp.repository.ArtistGradeRepository;
 import com.musichub.musichubapp.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,21 +13,43 @@ import java.util.Optional;
 @Service
 public class DbArtistService {
 
-// tworzę obiekt ArtistRepository po to, aby zaciągnąć metody z tej klasy.
     @Autowired
     ArtistRepository artistRepository;
 
+    @Autowired
+    ArtistGradeRepository artistGradeRepository;
+
     public List<Artist> getAllArtists(){
+
         return artistRepository.findAll();
     }
 
     public Optional<Artist> getArtist(Integer id){
+
         return artistRepository.findById(id);
     }
     public Artist saveArtist(final Artist artist){
+
         return artistRepository.save(artist);
     }
     public void deleteArtist(Integer id){
+
         artistRepository.deleteById(id);
     }
+
+    public List<ArtistGrade> getAllArtistGrades(){
+        return artistGradeRepository.findAll();
+    }
+
+    public Optional<ArtistGrade> getArtistGrade(Integer id){
+        return artistGradeRepository.findById(id);
+    }
+
+    public ArtistGrade saveArtistGrade(final ArtistGrade artistGrade){
+        return  artistGradeRepository.save(artistGrade);
+    }
+    public void deleteArtistGrade (Integer id){
+        artistGradeRepository.deleteById(id);
+    }
+
 }
