ac25e71fc2bb32feee69b5171c098dd83291188c
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-22 20:17:26 +0200

    added new field - ArtistGradeMapper; added new methods: getGrades, getArtistGrade, deleteArtistGrade, updateArtistGrade, createArtistGrade.

--- src/main/java/com/musichub/musichubapp/controller/ArtistController.java
+++ src/main/java/com/musichub/musichubapp/controller/ArtistController.java
@@ -1,11 +1,12 @@
 package com.musichub.musichubapp.controller;
 
 import com.musichub.musichubapp.domain.ArtistDto;
+import com.musichub.musichubapp.domain.ArtistGradeDto;
 import com.musichub.musichubapp.domain.Dto.ResponseDto;
+import com.musichub.musichubapp.mapper.ArtistGradeMapper;
 import com.musichub.musichubapp.mapper.ArtistMapper;
 import com.musichub.musichubapp.service.ArtistService;
 import com.musichub.musichubapp.service.DbArtistService;
-import com.musichub.musichubapp.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +18,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/artist")
 public class ArtistController { 
 
-// Tworzę obiekt DbArtistService, żeby zaciągnąć metody z klasy, DbArtistService
     @Autowired
     private ArtistService artistService;
 
@@ -27,6 +27,9 @@ public class ArtistController {
     @Autowired
     private ArtistMapper artistMapper;
 
+    @Autowired
+    private ArtistGradeMapper artistGradeMapper;
+
     @GetMapping("/{name}")
     public ResponseDto getArtistDetails(@PathVariable("name") String name){
         return artistService.getDetails(name);
@@ -34,6 +37,7 @@ public class ArtistController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getArtists")
     public List<ArtistDto> getArtists(){
+
         return artistMapper.mapToArtistDtoList(dbArtistService.getAllArtists());
     }
 
@@ -45,6 +49,7 @@ public class ArtistController {
         public void deleteArtist(@RequestParam Integer artistId){
             dbArtistService.deleteArtist(artistId);
         }
+
     @RequestMapping(method = RequestMethod.PUT, value = "updateArtist")
     public ArtistDto updateArtist(@RequestBody ArtistDto artistDto){
         return artistMapper.mapToArtistDto(dbArtistService.saveArtist(artistMapper.mapToArtist(artistDto)));
@@ -53,5 +58,31 @@ public class ArtistController {
     public void createArtist(@RequestBody ArtistDto artistDto){
         dbArtistService.saveArtist(artistMapper.mapToArtist(artistDto));
     }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "getAllArtistGrades")
+    public List<ArtistGradeDto> getGrades(){
+        return artistGradeMapper.mapToArtistGradeDtoList(dbArtistService.getAllArtistGrades());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getArtistGrade")
+    public ArtistGradeDto getArtistGrade(@RequestParam Integer artistGradeId) throws ArtistGradeNotFoundException{
+        return artistGradeMapper.mapToArtistGradeDto(dbArtistService.getArtistGrade(artistGradeId).orElseThrow(ArtistGradeNotFoundException::new));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteArtistGrade")
+    public void deleteArtistGrade(@RequestParam Integer artistGradeId) {
+        dbArtistService.deleteArtistGrade(artistGradeId);
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "updateArtistGrade")
+    public ArtistGradeDto updateArtistGrade(@RequestBody ArtistGradeDto artistGradeDto){
+        return artistGradeMapper.mapToArtistGradeDto(dbArtistService.saveArtistGrade(artistGradeMapper.mapToArtistGrade(artistGradeDto)));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createArtistGrade", consumes = APPLICATION_JSON_VALUE)
+    public void createArtistGrade(@RequestBody ArtistGradeDto artistGradeDto){
+        dbArtistService.saveArtistGrade(artistGradeMapper.mapToArtistGrade(artistGradeDto));
+    }
+
 
 }
