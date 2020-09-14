915dd171b133da6532d6f1f6e658070208b9c4cc
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-14 22:55:42 +0200

    introduced Builder pattern to ResponseDto class

--- src/main/java/com/musichub/musichubapp/domain/Dto/ResponseDto.java
+++ src/main/java/com/musichub/musichubapp/domain/Dto/ResponseDto.java
@@ -5,18 +5,64 @@ import lombok.Getter;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ResponseDto {
+public final class ResponseDto {
     private final TicketMasterResponseDto tmMusicianInfo;
     private final SpotifyResponseDto spotifyMusicianInfo;
     private final WikipediaResponseDto wikiMusicianInfo;
     private final YouTubeResponseDto ytMusicianInfo;
 
-    public ResponseDto(TicketMasterResponseDto tmMusicianInfo, SpotifyResponseDto spotifyMusicianInfo, WikipediaResponseDto wikiMusicianInfo, YouTubeResponseDto ytMusicianInfo) {
+    public static class ResponseDtoBuilder {
+
+        private TicketMasterResponseDto tmMusicianInfo;
+        private SpotifyResponseDto spotifyMusicianInfo;
+        private WikipediaResponseDto wikiMusicianInfo;
+        private YouTubeResponseDto ytMusicianInfo;
+
+        public ResponseDtoBuilder tmMusicianInfo(TicketMasterResponseDto tmMusicianInfo){
+            this.tmMusicianInfo = tmMusicianInfo;
+            return this;
+        }
+        public ResponseDtoBuilder spotifyMusicianInfo(SpotifyResponseDto spotifyMusicianInfo){
+            this.spotifyMusicianInfo = spotifyMusicianInfo;
+            return this;
+        }
+
+        public ResponseDtoBuilder wikiMusicianInfo(WikipediaResponseDto wikiMusicianInfo){
+            this.wikiMusicianInfo = wikiMusicianInfo;
+            return this;
+        }
+
+        public ResponseDtoBuilder ytMusicianInfo(YouTubeResponseDto ytMusicianInfo){
+            this.ytMusicianInfo = ytMusicianInfo;
+            return this;
+        }
+        public ResponseDto build(){
+            return new ResponseDto(tmMusicianInfo, spotifyMusicianInfo, wikiMusicianInfo, ytMusicianInfo);
+        }
+
+    }
+
+    public ResponseDto(final TicketMasterResponseDto tmMusicianInfo, final SpotifyResponseDto spotifyMusicianInfo, final WikipediaResponseDto wikiMusicianInfo, final YouTubeResponseDto ytMusicianInfo) {
         this.tmMusicianInfo = tmMusicianInfo;
         this.spotifyMusicianInfo = spotifyMusicianInfo;
         this.wikiMusicianInfo = wikiMusicianInfo;
         this.ytMusicianInfo = ytMusicianInfo;
 
     }
+    
+    public TicketMasterResponseDto getTmMusicianInfo() {
+        return tmMusicianInfo;
+    }
 
+    public SpotifyResponseDto getSpotifyMusicianInfo() {
+        return spotifyMusicianInfo;
+    }
+
+    public WikipediaResponseDto getWikiMusicianInfo() {
+        return wikiMusicianInfo;
+    }
+
+    public YouTubeResponseDto getYtMusicianInfo() {
+        return ytMusicianInfo;
+    }
 }
