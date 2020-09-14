7916824d23fbdf80ff0eec79fefa56b1f16650fe
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-14 22:41:12 +0200

    added new method - findFirstById.

--- src/main/java/com/musichub/musichubapp/repository/ArtistRepository.java
+++ src/main/java/com/musichub/musichubapp/repository/ArtistRepository.java
@@ -2,7 +2,6 @@ package com.musichub.musichubapp.repository;
 import com.musichub.musichubapp.entities.Artist;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -17,4 +16,6 @@ public interface ArtistRepository extends CrudRepository<Artist, Integer> {
     Artist save(Artist artist);
     @Override
     void deleteById(Integer id);
+
+    List<Artist> findFirstById(int id);
 }
