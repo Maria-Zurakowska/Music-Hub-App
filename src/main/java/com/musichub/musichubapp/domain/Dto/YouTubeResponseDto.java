fa3c415918f014db459fe8a3f0410c7bb80e07bf
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-30 20:07:36 +0200

    added @AllArgsConstructor

--- src/main/java/com/musichub/musichubapp/domain/Dto/YouTubeResponseDto.java
+++ src/main/java/com/musichub/musichubapp/domain/Dto/YouTubeResponseDto.java
@@ -1,6 +1,7 @@
 package com.musichub.musichubapp.domain.Dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class YouTubeResponseDto {
 
