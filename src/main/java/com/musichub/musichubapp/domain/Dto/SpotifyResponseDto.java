388afa9ec451439a0472b2413578e1fc2436dd27
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-30 20:02:23 +0200

    added @AllArgsConstructor

--- src/main/java/com/musichub/musichubapp/domain/Dto/SpotifyResponseDto.java
+++ src/main/java/com/musichub/musichubapp/domain/Dto/SpotifyResponseDto.java
@@ -1,12 +1,14 @@
 package com.musichub.musichubapp.domain.Dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
 public class SpotifyResponseDto {
 
     private SpotifyArtistDto artists;
