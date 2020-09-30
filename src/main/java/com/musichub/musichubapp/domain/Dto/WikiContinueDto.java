80cf1edce313134a1aa2486b03a2f14b753dcf27
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-30 20:08:01 +0200

    added @AllArgsConstructor

--- src/main/java/com/musichub/musichubapp/domain/Dto/WikiContinueDto.java
+++ src/main/java/com/musichub/musichubapp/domain/Dto/WikiContinueDto.java
@@ -1,11 +1,13 @@
 package com.musichub.musichubapp.domain.Dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WikiContinueDto {
 
