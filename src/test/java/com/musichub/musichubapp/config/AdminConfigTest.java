737d7af6db8c57ab4b6223cd142136c61eaa7adb
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-30 20:10:09 +0200

    added AdminConfigTest (test)

---
+++ src/test/java/com/musichub/musichubapp/config/AdminConfigTest.java
@@ -0,0 +1,14 @@
+package com.musichub.musichubapp.config;
+
+import org.junit.Test;
+
+public class AdminConfigTest {
+
+    private AdminConfig adminConfigUnderTest;
+
+    @Test
+    public void setUp() {
+
+        adminConfigUnderTest = new AdminConfig();
+    }
+}
